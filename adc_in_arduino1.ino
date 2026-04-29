aed6c2709e646c95742159ee26906c32757a7c04
Devisaisusmitha <devikanisetty661@gmail.com>
2026-04-30 00:53:12 +0530

    Add files via upload
    Simple demonstration of ADC in arduino

---
+++ adc_in_arduino1.ino
@@ -0,0 +1,24 @@
+// C++ code
+//
+void setup()
+{
+  //seria monitor
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  int sensorReading= analogRead(A0);
+  
+  Serial.println(sensorReading);
+  
+  int outputPitch = map(sensorReading, 0, 1023, 250, 5000);
+  
+  if(sensorReading!=0){
+    tone(13,outputPitch);
+  }else{
+      noTone(13);
+    }
+    delay(10);
+}
